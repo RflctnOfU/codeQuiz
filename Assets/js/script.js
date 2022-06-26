02a03b4cbb2814873f8f1bef97c45293c4155bd1
Kris Marshall <rflctnofu@hotmail.com>
2022-06-25 23:49:33 -0400

    got start page working and a button with eventlistener

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,8 @@ var score;
 
 const penalty = 5;
 
+var timeRemaining = 75;
+
 var navElement = document.getElementById("nav");
 
 var timerElement = document.getElementById("timer");
@@ -12,7 +14,6 @@ var questElement = document.getElementById("questions");
 
 var answerElement = document.getElementById("answer");
 
-var timeRemaining = 75;
 // timer function
 function setTimer () {
     var timerInterval = setInterval(function() {
@@ -23,18 +24,32 @@ function setTimer () {
             timerElement.textContent = "";
             // call function going to score page
         }
-
-        // if (help = false) {
-        //    setTimer();
-        // }
-        // function answerFalse(){
-        // timeRemaining -= penalty;
-        // setTimer();
-        // // return;
-        // }   
-        
+ 
     }, 1000);
 }
+
+    var heading = document.getElementById("heading");
+    var intro = document.getElementById("description");
+    var begin = document.getElementById("begin");
+    begin.addEventListener("click", startQuiz);
+
+    function startPage() {
+    heading.textContent = "Coding Quiz Challenge";
+    heading.setAttribute("style", "font-size: 2rem; font-weight: bold; margin-bottom: 20px");
+    intro.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
+    begin.textContent = "Start Quiz";
+    begin.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; margin-top: 10px;")
+    }
+
+    function startQuiz() {
+        heading.textContent = "";
+        heading.removeAttribute("style");
+        intro.textContent = "";
+        begin.textContent = "";
+        begin.removeAttribute("style");
+    }
+
+    startPage();
     setTimer();
     // penalty code
    /* if (help === false) {
