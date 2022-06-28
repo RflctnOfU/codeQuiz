80d4be4a30308de74a013eccb4a55c53853680b9
Kris Marshall <rflctnofu@hotmail.com>
2022-06-28 19:52:35 -0400

    finished questions and form, but ran into a timer bug...need to fix that. Starting on highscore page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 const penalty = 10;
 
-var timeRem = 2;
-
 var timeRemaining = 75;
+
+var form = document.getElementById("form")
 
 var navElement = document.getElementById("nav");
 
@@ -42,58 +42,84 @@ const button3 = document.createElement('button', { is : 'button3'});
 
 const button4 = document.createElement('button', { is : 'button4'});
 
+function answerTimeout () {
+    answerElement.textContent = '';
+    answerElement.removeAttribute("style")
+}
+
 function correctAnswer() {
-    var timeInt = setInterval(function() {
-        timeRem--;
-        answerElement.textContent = "Correct answer! ✅";
-        answerElement.setAttribute("style", "font-style: oblique; border-top: solid 1px; width: 50vw; padding: 10px 0;");
-        if (timeRem === 0) {
-            clearInterval(timeInt)
-            answerElement.textContent = "";
-            answerElement.removeAttribute("style");
-        }
-    }, 1000);
-    
+    answerElement.textContent = "Correct answer! ✅";
+    answerElement.setAttribute("style", "font-style: oblique; border-top: solid 1px; width: 50vw; padding: 10px 0;");
+    const timeoutDisplay = setTimeout(answerTimeout, 3000);
+}
+
+function wrongAnswer() {
+    answerElement.textContent = "Wrong answer! ❌";
+    answerElement.setAttribute("style", "font-style: oblique; border-top: solid 1px; width: 50vw; padding: 10px 0;");
+    const timeoutDisplay = setTimeout(answerTimeout, 3000);
+    // timeRemaining = timeRemaining - 10;
 }
 
 function a() {
     correctAnswer();
-    timeRem = 2;
-    console.log(timeRem)
-}
-
-function wrongAnswer() {
-    // update div to say correct answer
-    var timeInt2 = setInterval(function() {
-        timeRem--;
-        answerElement.textContent = "Wrong answer! ❌";
-        answerElement.setAttribute("style", "font-style: oblique; border-top: solid 1px; width: 50vw; padding: 10px 0;");
-        // call function to show next quest??
-        if (timeRem === 0) {
-            clearInterval(timeInt2)
-            answerElement.textContent = "";
-            answerElement.removeAttribute("style");
-        }
-    }, 1000);
-    timeRemaining -= penalty;
+    questionTwo();
 }
 
 function b() {
     wrongAnswer();
-    timeRem = 2;
-    console.log(timeRem)
+    questionTwo();
 }
+
+function c() {
+    correctAnswer();
+    questionThree();
+}
+
+function d() {
+    wrongAnswer();
+    questionThree();
+}
+
+function e() {
+    correctAnswer();
+    questionFour();
+}
+
+function f() {
+    wrongAnswer();
+    questionFour();
+}
+
+function g() {
+    correctAnswer();
+    questionFive();
+}
+
+function h() {
+    wrongAnswer();
+    questionFive();
+}
+
+function i() {
+    correctAnswer();
+    enterInitials();
+}
+
+function j() {
+    wrongAnswer();
+    enterInitials();
+}
+
 // timer function
 function setTimer () {
     var timerInterval = setInterval(function() {
         timeRemaining--;
         timerElement.textContent = "Timer: " + timeRemaining;
-        if (timeRemaining === 0) {
+        if (timeRemaining <= 0) {
             clearInterval(timerInterval)
             timerElement.textContent = "";
-            // call function going to score page
+            enterInitials();
         }
- 
     }, 1000);
 }
 
@@ -104,23 +130,23 @@ function appendBtns() {
     four.appendChild(button4);
 }
 
-// function removeBtns() {
-//     one.removeChild(button1);
-//     two.removeChild(button2);
-//     three.removeChild(button3);
-//     four.removeChild(button4);
-// }
+function removeBtns() {
+    one.removeChild(button1);
+    two.removeChild(button2);
+    three.removeChild(button3);
+    four.removeChild(button4);
+}
 
 function init() {
     heading.textContent = "Coding Quiz Challenge";
     heading.setAttribute("style", "font-size: 2rem; font-weight: bold; margin-bottom: 20px");
     intro.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
     button0.textContent = "Start Quiz";
-    button0.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px;");
+    button0.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px; margin: 10px;");
     begin.appendChild(button0);
     button0.addEventListener("click", questionOne);
 }
-        // question 1 function
+// question functions
 function questionOne() {
     setTimer();
     heading.textContent = "";
@@ -134,6 +160,7 @@ function questionOne() {
     questions.setAttribute("style", "font-size: 1.5rem; font-weight: bold; margin-bottom: 20px; text-align: start;");
     appendBtns();
     button1.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px 10px 5px 5px;");
+    button1.setAttribute("id", "one")
     button2.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px 10px 5px 5px;");
     button3.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px 10px 5px 5px;");
     button4.setAttribute("style", "background-color: rgb(100, 65, 147); color: white; border-radius: 7px; padding: 5px 10px 5px 5px;");
@@ -143,104 +170,79 @@ function questionOne() {
     button4.textContent = "4. Numbers";
     button1.addEventListener("click", b);
     button2.addEventListener("click", b);
-    button3.addEventListener("click", a)
+    button3.addEventListener("click", a);
     button4.addEventListener("click", b);
-    // if (button3.click) {
-    //     questionTwo();
-    // } else if (button1.click() || button2.click() || button4.click()) {
-    //     questionTwo();
-    // }
-    if (button3.addEventListener("click", questionTwo)) {
-        correctAnswer();
-        console.log("here we go")
-    } else if ((button1.addEventListener("click", questionTwo)) || (button2.addEventListener("click", questionTwo)) || (button4.addEventListener("click", questionTwo))) {
-        wrongAnswer();
-    }
 }
 
 function questionTwo() {
-    timeRem = 2;
     questions.textContent = "The condition in an if/else statement is enclosed within____.";
     button1.textContent = "1. Quotes";
     button2.textContent = "2. Curly brackets";
     button3.textContent = "3. Parentheses";
     button4.textContent = "4. Square brackets";
-    button1.addEventListener("click", b);
-    button2.addEventListener("click", b);
-    button3.addEventListener("click", a);
-    button4.addEventListener("click", b);
-    if (button3.addEventListener("click", questionThree)) {
-        a();
-    } else if ((button1.addEventListener("click", questionThree)) || (button2.addEventListener("click", questionThree)) || (button4.addEventListener("click", questionThree))) {
-        b();
-    }
+    button1.addEventListener("click", d);
+    button2.addEventListener("click", d);
+    button3.addEventListener("click", c);
+    button4.addEventListener("click", d);
 }
 
 function questionThree() {
-    timeRem = 2;
     questions.textContent = "Arrays in JavaScript can be used to store ____.";
     button1.textContent = "1. Numbers and Strings";
     button2.textContent = "2. Other Arrays";
     button3.textContent = "3. Booleans";
     button4.textContent = "4. All of the above";
-    // button1.addEventListener("click", b);
-    // button2.addEventListener("click", b);
-    button3.removeEventListener("click", a);
-    button3.addEventListener("click", b);
-    button4.removeEventListener("click", b);
-    button4.addEventListener("click", a);
-    if (button4.addEventListener("click", questionFour)) {
-        a();
-    } else if ((button1.addEventListener("click", questionFour)) || (button2.addEventListener("click", questionFour)) || (button3.addEventListener("click", questionFour))) {
-        b();
-    }
+    button1.addEventListener("click", f);
+    button2.addEventListener("click", f);
+    button3.addEventListener("click", f);
+    button4.addEventListener("click", e);
 }
 
 function questionFour() {
-    timeRem = 2;
     questions.textContent = "String values must be enclosed within ____ when being assigned to variables.";
     button1.textContent = "1. Commas";
     button2.textContent = "2. Curly Brackets";
     button3.textContent = "3. Quotes";
     button4.textContent = "4. Parentheses";
-    // button1.addEventListener("click", b);
-    // button2.addEventListener("click", b);
-    button3.removeEventListener("click", b);
-    button3.addEventListener("click", a);
-    button4.removeEventListener("click", a);
-    button4.addEventListener("click", b);
-    if (button3.addEventListener("click", questionFive)) {
-        a();
-    } else if ((button1.addEventListener("click", questionFive)) || (button2.addEventListener("click", questionFive)) || (button4.addEventListener("click", questionFive))) {
-        b();
-    }
+    button1.addEventListener("click", h);
+    button2.addEventListener("click", h);
+    button3.addEventListener("click", g);
+    button4.addEventListener("click", h);
 }
 
 function questionFive() {
-    timeRem = 2;
     questions.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
     button1.textContent = "1. JavaScript";
     button2.textContent = "2. Terminal/Bash";
     button3.textContent = "3. for loops";
     button4.textContent = "4. console.log";
-    // button1.addEventListener("click", b);
-    // button2.addEventListener("click", b);
-    button3.removeEventListener("click", a);
-    button3.addEventListener("click", b);
-    button4.removeEventListener("click", b);
-    button4.addEventListener("click", a);
-    if (button4.addEventListener("click", enterInitials)) {
-        a();
-    } else if ((button1.addEventListener("click", enterInitials)) || (button2.addEventListener("click", enterInitials)) || (button3.addEventListener("click", enterInitials))) {
-        b();
-    }
+    button1.addEventListener("click", j);
+    button2.addEventListener("click", j);
+    button3.addEventListener("click", j);
+    button4.addEventListener("click", i);
 }
 
 function enterInitials() {
+    removeBtns();
+    var label = document.createElement('label');
+    var submit = document.createElement('input');
+    var input = document.createElement('input');
+    label.setAttribute("for", "input");
+    label.textContent = "Enter Initials: "
+    input.setAttribute('name', 'input');
+    input.setAttribute('type', 'text');
+    submit.setAttribute('type', 'submit');
+    submit.setAttribute('value', 'Submit');
+    submit.setAttribute('style', 'background-color: rgb(100, 65, 147); color: white; border-radius: 7px; margin: 0 10px;');
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(submit);
     console.log("hello")
+    questions.textContent = "All Done!"
+    intro.textContent = "Your final score is " + timeRemaining;
+    intro.setAttribute('style', 'margin-bottom: 10px;')
 }
     init();
-
     // submit initials function
 
     // highscores function
